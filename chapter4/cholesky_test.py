1f154cdbac6c6e898cb897fd9884da8f3cf30d6e
Afroz Mohiuddin <afrozenator@gmail.com>
2024-06-10 08:29:42 -0700

    add test for ldlt

--- chapter4/cholesky_test.py
+++ chapter4/cholesky_test.py
@@ -5,15 +5,6 @@ import unittest
 
 
 class CholeskyTest(unittest.TestCase):
-  def test_cholesky(self):
-    A = np.array([
-        [4., -2., 4.],
-        [-2., 5., -4.],
-        [4., -4., 14.]
-    ], dtype=np.float64)
-    L, mI = cd.cholesky_decomposition(A)
-    np.testing.assert_allclose(A, L @ L.T)
-    np.testing.assert_allclose(mI, np.eye(A.shape[0]))
 
   def test_elimination_matrix(self):
     A = np.array([
@@ -31,6 +22,27 @@ class CholeskyTest(unittest.TestCase):
     maybe_I = E @ E_inv
     # print(f'{maybe_I=}')
     np.testing.assert_allclose(maybe_I, np.eye(A.shape[0]))
+
+  def test_cholesky(self):
+    A = np.array([
+        [4., -2., 4.],
+        [-2., 5., -4.],
+        [4., -4., 14.]
+    ], dtype=np.float64)
+    L, I = cd.cholesky_decomposition(A)
+    np.testing.assert_allclose(A, L @ L.T)
+    np.testing.assert_allclose(np.eye(A.shape[0]), I)
+
+  def test_ldlt(self):
+    A = np.array([
+        [4., -2., 4.],
+        [-2., 5., -4.],
+        [4., -4., 14.]
+    ], dtype=np.float64)
+    L, D = cd.ldlt_decomposition(A)
+    # Assert that D is diagonal.
+    np.testing.assert_allclose(D, np.diag(D) * np.eye(D.shape[0]))
+    np.testing.assert_allclose(A, L @ D @ L.T)
 
 
 if __name__ == '__main__':
