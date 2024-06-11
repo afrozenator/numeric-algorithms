ca1362803181eb46a388c9dd10ee062ba79438e6
Afroz Mohiuddin <afrozenator@gmail.com>
2024-06-11 08:32:03 -0700

    update the test

--- chapter4/cholesky_test.py
+++ chapter4/cholesky_test.py
@@ -12,16 +12,10 @@ class CholeskyTest(unittest.TestCase):
         [-2., 5., -4.],
         [4., -4., 14.]
     ], dtype=np.float64)
-    # print(f'{A=}')
     E = cd.cholesky_elimination_matrix(A)
-    # print(f'{E=}')
-    # A1 = E @ A @ E.T
-    # print(f'E @ A @ E.T: {A1}')
     E_inv = cd.cholesky_elimination_matrix_inverse(A)
-    # print(f'{E_inv=}')
-    maybe_I = E @ E_inv
-    # print(f'{maybe_I=}')
-    np.testing.assert_allclose(maybe_I, np.eye(A.shape[0]))
+    I = E @ E_inv
+    np.testing.assert_allclose(I, np.eye(A.shape[0]))
 
   def test_cholesky(self):
     A = np.array([
